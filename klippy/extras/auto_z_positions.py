220486e9fed09c2e9f86250b153dcfbe854e21b3
Islam Sharabash <islam.sharabash@gmail.com>
2023-01-12 20:41:01 -0800

    PAUSED: Use `git resume` to continue working.

--- klippy/extras/auto_z_positions.py
+++ klippy/extras/auto_z_positions.py
@@ -5,105 +5,111 @@ import logging
 
 # ref: https://docs.sympy.org/latest/modules/geometry/plane.html
 class AutoZPositions:
-  def __init__(self, config):
-    # use ProbePointsHelper to validate config
-    helper = probe.ProbePointsHelper(config, lambda *args: None)
-    helper.minimum_points(3)
+    def __init__(self, config):
+        # use ProbePointsHelper to validate config
+        helper = probe.ProbePointsHelper(config, lambda *args: None)
+        helper.minimum_points(3)
 
-    self.printer = config.get_printer()
-    self.config = config
-    # points = config.get('points', None)
-    self.gcode = self.printer.lookup_object('gcode')
-    self.z_steppers = []
+        # TODO(ibash) get lift speed from probe
+        # TODO(ibash) get distance from config, default to 3
 
-    # TODO(ibash) add helper descriptions for commands
-    self.gcode.register_command('AUTO_Z_POSITIONS', self.cmd_AUTO_Z_POSITIONS)
-    self.gcode.register_command('PROBE_PLANE', self.cmd_PROBE_PLANE)
-    self.printer.register_event_handler("klippy:connect", self.handle_connect)
+        self.printer = config.get_printer()
+        self.config = config
+        self.gcode = self.printer.lookup_object('gcode')
+        self.z_steppers = []
 
-  def handle_connect(self):
-      kin = self.printer.lookup_object('toolhead').get_kinematics()
-      z_steppers = [s for s in kin.get_steppers() if s.is_active_axis('z')]
-      if len(z_steppers) != 3:
-          raise self.printer.config_error("%s needs exactly 3 z steppers" % self.config.get_name())
-      self.z_steppers = z_steppers
+        # TODO(ibash) add helper descriptions for commands
+        self.gcode.register_command('AUTO_Z_POSITIONS',
+                                    self.cmd_AUTO_Z_POSITIONS)
+        self.gcode.register_command('PROBE_PLANE', self.cmd_PROBE_PLANE)
+        self.printer.register_event_handler("klippy:connect",
+                                            self.handle_connect)
 
-  def cmd_AUTO_Z_POSITIONS(self, gcmd):
-    z_positions = asyncio.run(self.auto_z_positions(gcmd))
-    gcmd.respond_info("z positions: %s" % z_positions)
+    def handle_connect(self):
+        kin = self.printer.lookup_object('toolhead').get_kinematics()
+        z_steppers = [s for s in kin.get_steppers() if s.is_active_axis('z')]
+        if len(z_steppers) != 3:
+            raise self.printer.config_error("%s needs exactly 3 z steppers" %
+                                            self.config.get_name())
+        self.z_steppers = z_steppers
 
-  def cmd_PROBE_PLANE(self, gcmd):
-    plane = asyncio.run(self.probe_plane(gcmd))
-    gcmd.respond_info("probed plane: %s" % plane)
+    def cmd_AUTO_Z_POSITIONS(self, gcmd):
+        z_positions = asyncio.run(self.auto_z_positions(gcmd))
+        gcmd.respond_info("z positions: %s" % z_positions)
 
-  async def auto_z_positions(self, gcmd):
-    # algorithm
-    # 1. probe a base plane (plane 1)
-    # 2. for each z stepper:
-    #   1. move the z stepper
-    #   2. probe a plane (planes 2-4)
-    #   3. move the z stepper back
-    # 3. planes 2-4 intersected with the base plane (plane 1) produce a line
-    # 4. each line intersected with the other produce a pivot point
-    base = await self.probe_plane(gcmd)
-    distance = 3
-    planes = [base]
-    for stepper in self.z_steppers:
-      self.move_z_stepper(stepper, distance)
-      plane = await self.probe_plane(gcmd)
-      planes.append(plane)
-      self.move_z_stepper(stepper, -1 * distance)
+    def cmd_PROBE_PLANE(self, gcmd):
+        plane = asyncio.run(self.probe_plane(gcmd))
+        gcmd.respond_info("probed plane: %s" % plane)
 
-    z_positions = self.compute_z_positions(planes)
-    return z_positions
+    async def auto_z_positions(self, gcmd):
+        # algorithm
+        # 1. probe a base plane (plane 1)
+        # 2. for each z stepper:
+        #   1. move the z stepper
+        #   2. probe a plane (planes 2-4)
+        #   3. move the z stepper back
+        # 3. planes 2-4 intersected with the base plane (plane 1) produce a line
+        # 4. each line intersected with the other produce a pivot point
+        base = await self.probe_plane(gcmd)
+        distance = 3
+        planes = [base]
+        for stepper in self.z_steppers:
+            self.move_z_stepper(stepper, distance)
+            plane = await self.probe_plane(gcmd)
+            planes.append(plane)
+            self.move_z_stepper(stepper, -1 * distance)
 
-  async def probe_plane(self, gcmd):
-    result = await self.probe_async(gcmd)
-    points = result["points"]
-    plane = Plane(points[0], points[1], points[2])
-    return plane
+        z_positions = self.compute_z_positions(planes)
+        return z_positions
 
-  def probe_async(self, gcmd):
-    future = asyncio.Future()
-    def finalize(offsets, positions):
-      # TODO(ibash) should the x and y here be adjusted by offsets?
-      points = [ Point(x, y, z) for (x, y, z) in positions]
-      future.set_result({
-        "offsets": offsets,
-        "points": points,
-        "positions": positions,
-      })
+    async def probe_plane(self, gcmd):
+        result = await self.probe_async(gcmd)
+        points = result["points"]
+        plane = Plane(points[0], points[1], points[2])
+        return plane
 
-    helper = probe.ProbePointsHelper(self.config, finalize)
-    helper.minimum_points(3)
-    helper.start_probe(gcmd)
+    def probe_async(self, gcmd):
+        future = asyncio.Future()
 
-    return future
+        def finalize(offsets, positions):
+            # TODO(ibash) should the x and y here be adjusted by offsets?
+            points = [Point(x, y, z) for (x, y, z) in positions]
+            future.set_result({
+                "offsets": offsets,
+                "points": points,
+                "positions": positions,
+            })
 
-  def move_z_stepper(self, stepper, distance):
-    # TODO(ibash) stepper = "stepper_z", "stepper_z1", "stepper_z2"
-    # TODO(ibash) get speed from probe lift_speed
-    speed = 3
+        helper = probe.ProbePointsHelper(self.config, finalize)
+        helper.minimum_points(3)
+        helper.start_probe(gcmd)
 
-    stepper_name = stepper.get_name()
-    # distance = 10
-    self.gcode.run_script_from_command(
-        "FORCE_MOVE STEPPER=%s DISTANCE=%.3f VELOCITY=%.3f"  % (stepper_name, distance, speed))
+        return future
 
-  def compute_z_positions(self, planes):
-    lines = [
-      planes[0].intersection(planes[1]),
-      planes[0].intersection(planes[2]),
-      planes[0].intersection(planes[3])
-    ]
+    def move_z_stepper(self, stepper, distance):
+        # TODO(ibash) stepper = "stepper_z", "stepper_z1", "stepper_z2"
+        # TODO(ibash) get speed from probe lift_speed
+        speed = 3
 
-    points = [
-      lines[0].intersection(lines[1]),
-      lines[0].intersection(lines[2]),
-      lines[1].intersection(lines[2])
-    ]
+        stepper_name = stepper.get_name()
+        # distance = 10
+        self.gcode.run_script_from_command(
+            "FORCE_MOVE STEPPER=%s DISTANCE=%.3f VELOCITY=%.3f" %
+            (stepper_name, distance, speed))
 
-    return points
+    def compute_z_positions(self, planes):
+        lines = [
+            planes[0].intersection(planes[1]),
+            planes[0].intersection(planes[2]),
+            planes[0].intersection(planes[3])
+        ]
+
+        points = [
+            lines[0].intersection(lines[1]), lines[0].intersection(lines[2]),
+            lines[1].intersection(lines[2])
+        ]
+
+        return points
 
 def load_config(config):
-  return AutoZPositions(config)
+    return AutoZPositions(config)
